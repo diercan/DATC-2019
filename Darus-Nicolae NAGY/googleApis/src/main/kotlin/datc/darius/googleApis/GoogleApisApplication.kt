0942d85be0a365d10c5bac1e3536204fb11965a2
Darius Nagy <nagydarius97@gmail.com>
2019-10-22 18:39:10 +0300

    added all to repo

---
+++ Darus-Nicolae NAGY/googleApis/src/main/kotlin/datc/darius/googleApis/GoogleApisApplication.kt
@@ -0,0 +1,11 @@
+package datc.darius.googleApis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoogleApisApplication
+
+fun main(args: Array<String>) {
+	runApplication<GoogleApisApplication>(*args)
+}
